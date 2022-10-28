88d0ed42c56035c27d328647c34124d3377338da
Ngoc Kha <khanguyen.jobs@gmail.com>
2022-10-28 11:21:52 +0700

    add control direction and trail ball

--- Assets/Scrips/BallController.cs
+++ Assets/Scrips/BallController.cs
@@ -7,18 +7,22 @@ public class BallController : MonoBehaviour
     private GameObject player;
     private GameObject target;
     public float force = 18f;
-    private float bounceForce = .8f;
-    private Vector3 Direction;
+    private float bounceForce = .9f;
+    private Vector3 direction;
     private Vector3 startPos;
     private float timeUpdate;
     private float radius;
+    // Get mouse down and mouse up --> to detect direction of mouse swipe
+    
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
     void Start()
     {
         startPos = transform.position;
         player = GameObject.Find("Player");
         target = GameObject.Find("Target");
         radius = gameObject.GetComponent<SphereCollider>().radius;
-        Direction = (target.transform.position - player.transform.position)/(target.transform.position - player.transform.position).magnitude;
+        direction = (target.transform.position - player.transform.position)/(target.transform.position - player.transform.position).magnitude;
     }
 
     void Update()
@@ -30,9 +34,12 @@ public class BallController : MonoBehaviour
         */
 
         timeUpdate += Time.deltaTime;
-        transform.position = updatePositionOverTime(timeUpdate);
+        gameObject.transform.position = updatePositionOverTime(timeUpdate);
 
         int numberOfFrameToPredict = 2;
+        
+        // Get direction before hit ball
+        Vector3 dir = Swipe();
 
         // Use OverlapSphere to detect if the ball collide in the next frame
         Collider[] colliders = Physics.OverlapSphere(updatePositionOverTime(timeUpdate + Time.deltaTime * numberOfFrameToPredict), radius, 1);
@@ -44,8 +51,7 @@ public class BallController : MonoBehaviour
             // Player hit the ball
             if(colliders[0].transform.CompareTag("Player"))
             {
-                // add direction
-                Direction = (target.transform.position - player.transform.position)/(target.transform.position - player.transform.position).magnitude;
+                direction = dir;
             }
             // The ball go out
             else if(colliders[0].transform.CompareTag("destroy"))
@@ -73,7 +79,7 @@ public class BallController : MonoBehaviour
                     normal = new Vector3(0, 1, 0);
                 }
                 // Reflect vector of the ball
-                Direction = Vector3.Reflect(Direction, normal) * bounceForce;
+                direction = Vector3.Reflect(direction, normal) * bounceForce;
             }
         }
     }
@@ -81,6 +87,31 @@ public class BallController : MonoBehaviour
     // update position of the ball over time by Time.deltaTime
     Vector3 updatePositionOverTime(float t)
     {
-        return startPos + Direction * force * t + 0.5f * Physics.gravity * t * t;
+        return startPos + direction * force * t + 0.5f * Physics.gravity * t * t;
+    }
+
+    
+    public Vector3 Swipe()
+    {
+        // Get position mouse down and mouse up
+        if(Input.GetMouseButtonDown(0))
+        {
+            firstPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            secondPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        }
+        // Get direction of ball axis Y
+        Vector3 heigh = (target.transform.position - player.transform.position)/(target.transform.position - player.transform.position).magnitude;
+        // Get direction of mouse swipe --> direction of ball Axis X, Z (direction of mouse Axis X, Y)
+        Vector2 dir = (secondPressPos - firstPressPos)/(secondPressPos - firstPressPos).magnitude;
+        // if haven't swipe the mouse --> return direction from player to target
+        if(firstPressPos == Vector2.zero)
+        {
+            return (target.transform.position - player.transform.position)/(target.transform.position - player.transform.position).magnitude;
+        }
+        // return direction of mouse swipe
+        return new Vector3(dir.x, heigh.y, dir.y);
     }
 }
